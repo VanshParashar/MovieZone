b9560b082da915a8c7413debe559e564f0d789c6
primeeeeaa <vanshparashar31@gmail.com>
2025-09-10 19:46:29 +0530

    carousel ui changes

--- lib/ui/widgets/trending_carousel.dart
+++ lib/ui/widgets/trending_carousel.dart
@@ -1,12 +1,10 @@
 // lib/ui/widgets/trending_carousel.dart
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../data/models/movie_model.dart';
 import '../screens/details_screen.dart';
 import 'movie_card.dart';
 
-class TrendingCarousel extends StatelessWidget {
+class TrendingCarousel extends StatefulWidget {
   final List<MovieModel> movies;
   final void Function(int id, bool newVal) onToggleBookmark;
   final double cardWidth;
@@ -21,51 +19,83 @@ class TrendingCarousel extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<TrendingCarousel> createState() => _TrendingCarouselState();
+}
+
+class _TrendingCarouselState extends State<TrendingCarousel> {
+  late final PageController _controller;
+  double _page = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(
+      viewportFraction: 0.65,
+    )..addListener(() {
+        setState(() {
+          _page = _controller.page ?? 0.0;
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  double _scaleFor(int index) {
+    final diff = (_page - index).abs();
+    return (1 - (diff * 0.15)).clamp(0.85, 1.0);
+  }
+
+  double _offsetYFor(int index) {
+    final diff = (_page - index).abs();
+    return diff < 1 ? (diff * 20) : 20;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (movies.isEmpty) {
+    if (widget.movies.isEmpty) {
       return SizedBox(
-        height: cardHeight,
-        child: const Center(
-          child: Text(
-            'No trending movies found',
-            style: TextStyle(color: Colors.white70),
-          ),
-        ),
+        height: widget.cardHeight,
+        child: Center(child: Text('No trending movies found')),
       );
     }
-
     return SizedBox(
-      height: cardHeight,
-      child: CarouselSlider.builder(
-        itemCount: movies.length,
-        itemBuilder: (context, index, realIndex) {
-          final movie = movies[index];
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => DetailsScreen(movieId: movie.id),
+      height: widget.cardHeight,
+      child: PageView.builder(
+        controller: _controller,
+        padEnds: true,
+        itemCount: widget.movies.length,
+        itemBuilder: (context, index) {
+          final movie = widget.movies[index];
+          final scale = _scaleFor(index);
+          final offsetY = _offsetYFor(index);
+          return Transform.translate(
+            offset: Offset(0, offsetY),
+            child: Transform.scale(
+              scale: scale,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => DetailsScreen(movieId: movie.id),
+                    ),
+                  );
+                },
+                child: MovieCard(
+                  movie: movie,
+                  width: widget.cardWidth,
+                  height: widget.cardHeight,
+                  onBookmark: () =>
+                      widget.onToggleBookmark(movie.id, !movie.isBookmarked),
                 ),
-              );
-            },
-            child: MovieCard(
-              movie: movie,
-              width: cardWidth.w,
-              height: cardHeight.h,
-              onBookmark: () =>
-                  onToggleBookmark(movie.id, !movie.isBookmarked),
+              ),
             ),
           );
         },
-        options: CarouselOptions(
-          height: cardHeight.h,
-          enlargeCenterPage: true, // center movie thoda bada dikhega
-          autoPlay: false, // auto slide
-          autoPlayInterval: const Duration(seconds: 4),
-          viewportFraction: 0.6, // left-right peek
-          enableInfiniteScroll: true, // loop
-        ),
       ),
     );
   }
